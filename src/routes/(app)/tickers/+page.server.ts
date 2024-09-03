f931f33039755ded36d85115f09d2077a3948e29
VladLegkowski <glo.vlegkovskii@iberia.es>
2024-09-03 14:26:00 +0200

    typing

--- src/routes/(app)/tickers/+page.server.ts
+++ src/routes/(app)/tickers/+page.server.ts
@@ -2,6 +2,7 @@ import { fail, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import * as db from '$lib/server/db';
 import { plCalculationSchema } from '$lib/schemas';
+import type { Company } from '../../../lib/types';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url, fetch, locals }) => {
@@ -29,14 +30,7 @@ export const load: PageServerLoad = async ({ url, fetch, locals }) => {
 			exchangeShortName: string;
 		}> = (await response.json()) ?? [];
 
-		const companiesWithPrices: Array<{
-			symbol: string;
-			name: string;
-			currency: string;
-			stockExchange: string;
-			exchangeShortName: string;
-			price: number;
-		}> = await Promise.all(
+		const companiesWithPrices: Company[] = await Promise.all(
 			companies.map(async (company) => {
 				try {
 					const priceResponse = await fetch(
